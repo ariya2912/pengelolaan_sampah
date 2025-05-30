8b6cd22bdeeb380445177f57e38a7a93f33f8f54
ariya2912 <ariyafawaz29@gmail.com>
2025-05-31 02:46:04 +0700

    Initial commit

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,19 +1,33 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="#" class="brand-link"><span class="brand-text font-weight-light">Pengelolaan Sampah</span></a>
+    <a href="#" class="brand-link">
+        <span class="brand-text font-weight-light">Pengelolaan Sampah</span>
+    </a>
     <div class="sidebar">
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview">
                 <li class="nav-item">
-                    <a href="{{ url('/dashboard') }}" class="nav-link"><i class="nav-icon fas fa-tachometer-alt"></i> <p>Dashboard</p></a>
+                    <a href="{{ route('dashboard') }}" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ url('/pengaduan') }}" class="nav-link"><i class="nav-icon fas fa-exclamation-triangle"></i> <p>Pengaduan</p></a>
+                    <a href="{{ route('laporan.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-exclamation-triangle"></i>
+                        <p>Laporan Sampah</p>
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ url('/setoran') }}" class="nav-link"><i class="nav-icon fas fa-recycle"></i> <p>Setoran Bank Sampah</p></a>
+                    <a href="{{ route('setoran.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-recycle"></i>
+                        <p>Setoran Bank Sampah</p>
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ url('/pengangkutan') }}" class="nav-link"><i class="nav-icon fas fa-truck"></i> <p>Layanan Pengangkutan</p></a>
+                    <a href="{{ route('pengangkutan.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-truck"></i>
+                        <p>Layanan Pengangkutan</p>
+                    </a>
                 </li>
             </ul>
         </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,67 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', fn () => redirect('/dashboard'));
-
-Route::get('/dashboard', fn () => view('dashboard'));
-
-Route::get('/pengaduan', fn () => view('pengaduan.index'));
-Route::get('/setoran', fn () => view('setoran.index'));
-Route::get('/pengangkutan', fn () => view('pengangkutan.index'));
-
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LaporanSampahController;
 use App\Http\Controllers\SetoranSampahController;
 use App\Http\Controllers\PemesananLayananController;
 
+// Redirect root to dashboard
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
+
+// Form login
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+// Proses login
+Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'login']);
+
+// Logout
+Route::post('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
+// Group routes that require authentication
 Route::middleware(['auth'])->group(function () {
-    Route::resource('laporan_sampah', LaporanSampahController::class);
-    Route::resource('setoran_sampah', SetoranSampahController::class);
-    Route::resource('pemesanan_layanan', PemesananLayananController::class);
+
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Laporan Sampah (dulu: pengaduan)
+    Route::resource('laporan_sampah', LaporanSampahController::class)
+        ->names([
+            'index'   => 'laporan.index',
+            'create'  => 'laporan.create',
+            'store'   => 'laporan.store',
+            'show'    => 'laporan.show',
+            'edit'    => 'laporan.edit',
+            'update'  => 'laporan.update',
+            'destroy' => 'laporan.destroy',
+        ]);
+
+    // Setoran Bank Sampah
+    Route::resource('setoran_sampah', SetoranSampahController::class)
+        ->names([
+            'index'   => 'setoran.index',
+            'create'  => 'setoran.create',
+            'store'   => 'setoran.store',
+            'show'    => 'setoran.show',
+            'edit'    => 'setoran.edit',
+            'update'  => 'setoran.update',
+            'destroy' => 'setoran.destroy',
+        ]);
+
+    // Layanan Pengangkutan
+    Route::resource('pemesanan_layanan', PemesananLayananController::class)
+        ->names([
+            'index'   => 'pengangkutan.index',
+            'create'  => 'pengangkutan.create',
+            'store'   => 'pengangkutan.store',
+            'show'    => 'pengangkutan.show',
+            'edit'    => 'pengangkutan.edit',
+            'update'  => 'pengangkutan.update',
+            'destroy' => 'pengangkutan.destroy',
+        ]);
 });
